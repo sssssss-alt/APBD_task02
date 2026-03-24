d91bb6b4a87a82c0649c5ee5be573e811b9b425e
Orest Tryndyak <s32757@pjwstk.edu.pl>
2026-03-24 11:52:01 +0100

    Creating new branch Testing-Program.cs for testing purposes

--- Program.cs
+++ Program.cs
@@ -1,11 +1,36 @@
 ﻿
 
 using APBD_TASK2.Database;
+using APBD_TASK2.Enum;
 using APBD_TASK2.Models;
 
 var db = Singleton.Instance;
 
-var Laptop = new Laptop("Dell XPS 15", 2, 13);
-var Camera = new Camera("Sony15K", 2000, 100);
-var Projector = new Projector("Sonylul", 120, true);
-//db.Equipment.Add(Laptop);
+var equipmentService = new EquipmentService();
+var userService = new UserService();
+var rentalService = new RentalService();
+var reportService = new ReportService();
+
+equipmentService.AddEquipment(new Laptop("Dell XPS 15", 2, 13));
+equipmentService.AddEquipment(new Camera("Sony15K", 2000, 100));
+equipmentService.AddEquipment(new Projector("Sonylul", 120, true));
+
+
+userService.AddUser(new User("Orest", "Tryndyak", UserType.Student));
+userService.AddUser(new User("Dima", "Syrotkin", UserType.Teacher));
+
+rentalService.Rent(1, 1, 7);
+//invalid operation(that equipment aalready rented):
+try
+{
+    rentalService.Rent(2, 1, 7);
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Error: {e.Message}");
+}
+
+rentalService.ReturnEquipment(1);
+
+Console.WriteLine();
+reportService.GenerateReport();
